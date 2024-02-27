de5d4bbb0597c99679c9d1d8ac5f7a88dfe4e75e
RedfordRobert68 <redfordrobert68@gmail.com>
2024-02-26 23:03:32 -0500

    Part 2 of cart build

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link, useParams, useNavigate, useLocation } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import { Message } from '../components/Message'
+import { addToCart } from '../actions/cartActions'
 
 function CartScreen() {
+
+  const history = useNavigate()
+  const params = useParams()
+  const location = useLocation()
+
+  const productId = params.id
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1
+
+ const dispatch = useDispatch()
+
+ const cart = useSelector(state => state.cart)
+ const { cartItems } = cart
+ console.log('cartItems:', cartItems)
+
+ useEffect(() => {
+    if(productId) {
+        dispatch(addToCart(productId, qty))
+    }
+ }, [dispatch, productId, qty])
+
+
   return (
     <div>
         Cart
