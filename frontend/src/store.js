4275db28c2f431079bcfd6a674e5c256185377be
RedfordRobert68 <redfordrobert68@gmail.com>
2024-05-20 23:12:02 -0400

    Completio of shipping screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -18,10 +18,16 @@ const cartItemsFromStorage = localStorage.getItem('cartItems') ?
     JSON.parse(localStorage.getItem('cartItems')) : []
 
 const userInfoFromStorage = localStorage.getItem('userInfo') ?
-    JSON.parse(localStorage.getItem('userInfo')) : null;
+    JSON.parse(localStorage.getItem('userInfo')) : null
+
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress') ?
+    JSON.parse(localStorage.getItem('shippingAddress')) : {}
 
 export const initialState = {
-    cart: {cartItems: cartItemsFromStorage},
+    cart: {
+        cartItems: cartItemsFromStorage,
+        shippingAddress: shippingAddressFromStorage
+    },
     userLogin: {userInfo: userInfoFromStorage}
 }
 
